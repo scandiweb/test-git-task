cce4d78ab91dab0a6f959f52d7dfa316725247b1
Andrii Antoniuk <andrii.antoniuk@scandiweb.com>
2025-01-05 18:41:47 +0100

    Installed sample data, made sure local setup is flawless

--- cma.js
+++ cma.js
@@ -1,14 +1,32 @@
 /** @type {import('@scandipwa/magento-scripts').CMAConfiguration} */
 module.exports = {
     magento: {
-        first_name: 'Scandiweb',
-        last_name: 'Developer',
-        email: 'developer@scandipwa.com',
-        user: 'admin',
-        password: 'scandipwa123',
-        adminuri: 'admin',
-        mode: 'developer',
-        edition: 'community'
+        first_name: "Scandiweb",
+        last_name: "Developer",
+        email: "developer@scandipwa.com",
+        user: "admin",
+        password: "scandipwa123",
+        adminuri: "admin",
+        mode: "developer",
+        edition: "community",
     },
-    configuration: {}
-}
+    configuration: {
+        elasticsearch: {
+            env: {
+                ES_JAVA_OPTS: "-Xms4096m -Xmx4096m",
+            },
+        },
+        php: {
+            env: {
+                CONFIG__DEFAULT__CATALOG__SEARCH__ENGINE: "opensearch",
+                CONFIG__DEFAULT__CATALOG__SEARCH__OPENSEARCH_SERVER_HOSTNAME:
+                    "localhost",
+                CONFIG__DEFAULT__CATALOG__SEARCH__OPENSEARCH_SERVER_PORT:
+                    "9200",
+                CONFIG__DEFAULT__CATALOG__SEARCH__OPENSEARCH_INDEX_PREFIX:
+                    "magento2",
+                CONFIG__DEFAULT__CATALOG__SEARCH__OPENSEARCH_ENABLE_AUTH: "0",
+            },
+        },
+    },
+};
